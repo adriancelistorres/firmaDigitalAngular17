c4ddd8be401c9894936270c28beb791d123d8973
Adrian Celis Torres <adrian.celis@grupotawa.com>
2024-04-02 11:44:27 -0500

    ss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FirmaComponent } from './components/firma/firma.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', pathMatch: 'full', redirectTo: 'firma'
+    },
+    {
+        path: 'firma', component: FirmaComponent
+    }
+];
